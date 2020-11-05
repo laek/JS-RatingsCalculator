33aabadd05390752169f062272bb215d48bc000e
Laura <52936533+laek@users.noreply.github.com>
2020-11-05 17:15:37 +0000

    module 1 fixed tests

--- js/ratings.js
+++ js/ratings.js
@@ -1,13 +1,13 @@
-function collect_ratings = () => {
+const collect_ratings = () => {
   let ratings = {count:0, sum:0, average:0};
   let rating = 0;
 
   const elements = document.querySelectorAll('.rating');
-  elements.forEach = (element) => {
+  elements.forEach(element => {
     rating = parseInt(element.id.replace("star", ""));
     ratings.count += parseInt(element.value);
-    sum += (parseInt(element.value) * rating);
-  }
+    ratings.sum += (parseInt(element.value) * rating);
+  });
 
   if (ratings.count !== 0) {
     ratings.average = ratings.sum / ratings.count;
